d3788c82a249490d8ba75217d2097c56dc7eced6
吴逸豪 <303440188@qq.com>
2018-09-26 23:00:04 +0800

    删除最小元素或最大元素

--- binarySearchTree/BinarySearchTree.php
+++ binarySearchTree/BinarySearchTree.php
@@ -194,6 +194,97 @@ class BinarySearchTree
         }
     }
 
+
+    // 寻找二分搜索树的最小元素
+    public function minimum()
+    {
+        if ($this->size == 0) {
+            exit("BST is empty");
+        }
+
+        $minNode = $this->minimumNode($this->root);
+        return $minNode->e;
+    }
+
+    // 返回以node为根的二分搜索树的最小值所在的节点
+    private function minimumNode($node)
+    {
+        if ($node->left == null) {
+            return $node;
+        }
+        return $this->minimumNode($node->left);
+    }
+
+
+    // 寻找二分搜索树的最大元素
+    public function maximum()
+    {
+        if ($this->size == 0) {
+            exit("BST is empty");
+        }
+
+        $minNode = $this->maximumNode($this->root);
+        return $minNode->e;
+    }
+
+    // 返回以node为根的二分搜索树的最大值所在的节点
+    private function maximumNode($node)
+    {
+        if ($node->right == null) {
+            return $node;
+        }
+        return $this->maximumNode($node->right);
+    }
+
+    // 从二分搜索树中删除最小值所在节点, 返回最小值
+    public function removeMin()
+    {
+        $ret = $this->minimum();
+        $this->root = $this->removeMinNode($this->root);
+        return $ret;
+    }
+
+    // 删除掉以node为根的二分搜索树中的最小节点
+    // 返回删除节点后新的二分搜索树的根
+    private function removeMinNode($node)
+    {
+        if ($node->left == null) {
+            // 2 种情况
+            // 最小左子树节点元素，该节点下左右子树都为空;
+            // 最小左子树节点元素，该节点下左子树为空，右子树不为空，则需要把右子树 覆盖到 当前最小左子树节点元素上。
+            $rightNode = $node->right;
+            $node->right = null;
+            $this->size--;
+            return $rightNode;
+        }
+        $node->left = $this->removeMinNode($node->left);
+        return $node;
+    }
+
+    // 从二分搜索树中删除最大值所在节点
+    public function removeMax()
+    {
+        $ret = $this->maximum();
+        $this->root = $this->removeMaxNode($this->root);
+        return $ret;
+    }
+
+    // 删除掉以node为根的二分搜索树中的最大节点
+    // 返回删除节点后新的二分搜索树的根
+    private function removeMaxNode($node)
+    {
+        if ($node->right == null) {
+            $leftNode = $node->left;
+            $node->left = null;
+            $this->size--;
+            return $leftNode;
+        }
+        $node->right = $this->removeMaxNode($node->right);
+        return $node;
+    }
+
+
+
 }
 
 
