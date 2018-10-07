149e0c1140ae8d8f1792a3dc9aee8351bb238d7d
吴逸豪 <303440188@qq.com>
2018-10-07 21:15:50 +0800

    修改

--- map/Node.php
+++ map/Node.php
@@ -17,7 +17,7 @@ class Node
     public $value;
     public $next; //node
 
-    public function __construct($key, $value = null,  $next = null)
+    public function __construct($key = null, $value = null,  $next = null)
     {
         $this->key = $key;
         $this->value = $value;
